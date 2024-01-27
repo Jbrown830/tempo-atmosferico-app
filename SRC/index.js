3efb04212a902086d59123a656f70d9be4abe365
Jbrown830 <Jbrown830@gmail.com>
2024-01-27 12:15:00 -0500

    added weather data and changed name

--- SRC/index.js
+++ SRC/index.js
@@ -2,12 +2,34 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector ("#description");
+    let humidtyElement=document.querySelector("#humidty");
+    let windspeedElement=document.querySelector("#wind-speed");
+    let timeElement=document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
+
 
 
           cityElement.innerHTML = response.data.city;
+
+          timeElement.innerHTML=formatDate(date);
+          descriptionElement.innerHTML = response.data.condition.description;
+          humidtyElement.innerHTML= `${response.data.temperature.humidity}%`;
+          windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
           temperatureElement.innerHTML = Math.round(temperature);
 }
-
+function formatDate(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days  = [`Sunday`, `Monday`,`Tuesday`,`Wednsday`,`Thursday`,`Friday`,`Sarturday`,];
+    let day =days[date.getDay()];
+    if ( minutes < 10){
+        minutes = `0 ${minutes}`;
+    }
+    
+    return`${day}${hours}:${minutes}`;
+}
 
 
 function searchCity(city){
